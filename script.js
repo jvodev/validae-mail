f38cd084d4629d9289ce525f7ab7d232653fc91f
João Vitor <126678195+jvodev@users.noreply.github.com>
2023-04-21 17:34:45 -0300

    Add files via upload
    **atualização correta**, havia subido a versão errada!

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-//Desafio regras bellow
-
-/* deve ter o caractere @, deve ter pelomenos um . (ponto), não deve ter mais que 2 .(pontos) não pode ter, o ponto não pode ser junto do @
-não pode usar regx*/
-
-// model -> joao@smb.com
-
-// primario -> (@ .)
-// secundario -> (.@ @. @@ ..)
-
 const frm = document.querySelector("form")
 const calcular = document.getElementById("calcular")
 const res = document.getElementById("res")
@@ -18,19 +8,18 @@ frm.addEventListener("submit", (e)  =>  {
                                  
     const pegaArroba = textArroba.indexOf("@")                                              
     const pegaPonto = textArroba.indexOf(".") 
+    const pegaPonto2 = textArroba.indexOf(".", pegaArroba) + 1
 
     const maiorQueArroba = textArroba.includes('@@')
     const maiorQuePonto = textArroba.includes('..')
     const arrobaPonto = textArroba.includes('@.')
     const arrobaPonto2 = textArroba.includes('.@')
 
-//--------------------------------------------------------------------------------------------------------------------------------------------//
-
-if  (pegaArroba >= 1 && pegaPonto >= 1 && maiorQueArroba === false && maiorQuePonto === false && arrobaPonto === false && arrobaPonto2 === false)    {
-    res.innerText = `Parabéns o seu e-mail foi validado!`
-}
-    else    
+    if (pegaArroba >= 1 && pegaPonto >= 1 && pegaPonto2 > pegaArroba && maiorQueArroba === false && maiorQuePonto === false && arrobaPonto === false && arrobaPonto2 === false) {
+        res.innerText = `Parabéns o seu e-mail foi validado!`
+    } else {
         res.innerText = `E-mail Inválido.`
+    }
 
     e.preventDefault()
 })
